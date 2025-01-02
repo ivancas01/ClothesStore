110dc59f359f690e440c13ac1648616f73d80370
Ivan Torres <74121429+ivancas01@users.noreply.github.com>
2025-01-02 10:12:00 -0500

    Cambio de medidas en css

--- ProyectoPersonal/src/components/LastElements.jsx
+++ ProyectoPersonal/src/components/LastElements.jsx
@@ -1,17 +1,17 @@
 import { Link } from "react-router-dom";
 
-export function ElementsUnique({element, item}) {
-  return(
+export function ElementsUnique({ element, item }) {
+  return (
     <div className="elementsUnique">
-      <p className='elementsUniqueDropName'>{item.nombre}</p>
+      <p className="elementsUniqueDropName">{item.nombre}</p>
       <img src={element.imagen} alt={`Imagen de ${element.nombre}`} />
-      <p className='elementsUniquePrice'>Precio: ${element.precio} COL</p>
-      <p className='elementsUniqueSize'>Tallas: {element.tallas}</p>
-      {element.cantidad > 0 ? 
-        <h3 className='elementsUniqueName'>Ver producto</h3>
-      : 
-        <h3 className='elementsUniqueSold'>SOLDOUT</h3>
-      }
+      <p className="elementsUniquePrice">Precio: ${element.precio} COL</p>
+      <p className="elementsUniqueSize">Tallas: {element.tallas}</p>
+      {element.cantidad > 0 ? (
+        <h3 className="elementsUniqueName">Ver producto</h3>
+      ) : (
+        <h3 className="elementsUniqueSold">SOLDOUT</h3>
+      )}
     </div>
-  )
+  );
 }
